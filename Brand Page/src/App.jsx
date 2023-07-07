b5763c5b459502fbc5db5dd49a7e6edf44a81750
riteshjha <rj34567@gmail.com>
2023-07-08 01:26:55 +0530

    commit

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./Component/Hero";
+import Navigation from './Component/Navigation'
+
+const App = () =>{
+    return(
+        <div>
+           <Navigation />
+           <Hero /> 
+        </div>
+    )
+    
+    
+
+};
+export default App;
---
+++ Brand Page/src/Component/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () =>{
+    return (
+    <nav>
+    <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+    </div>
+    <div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contect</li>
+        </ul>
+    </div>
+
+    <button>Login</button>
+
+    </nav>)
+}
+export default Navigation;
